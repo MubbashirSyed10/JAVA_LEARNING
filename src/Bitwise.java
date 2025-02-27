6899c76efa2bbbfa35700d787537d0875340afaf
Mubbashir Syed <mubbashir.n@caratlane.com>
2025-02-27 21:57:27 +0530

    2nd

--- src/Bitwise.java
+++ src/Bitwise.java
@@ -2,5 +2,8 @@ public class Bitwise {
     public static void main(String [] arguments){
         int a = 5;
         System.out.println(Integer.toBinaryString(a));
+        if(a != 4){
+            System.out.println("true");
+        }
     }
 }
